ae32ba9aa7ac9ecce55be2eb8b5b9eea224adc68
thineththeekshana2017-creator <thineththeekshana2017@gmail.com>
2026-06-20 02:07:45 +0530

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,123 @@
+const lessonData = {
+    "Lesson 1": `
+어느	which
+사람	person
+이름	name
+국적	nationality
+누구	who
+학생	student
+경찰관	police officer
+의사	doctor
+기술자	technician
+선생님	teacher (honorific)
+소방관	firefighter
+간호사	nurse
+목수	carpenter
+씨	Mr./Ms.
+저	I
+아니요	no
+네	yes
+회사원	company employee
+공무원	public official
+요리사	cook
+농부	farmer
+근로자	worker
+점원	clerk
+운전기사	driver
+어부	fisherman
+    `,
+
+    "Lesson 2": `
+이것	this (thing)
+저것	that (thing over there)
+그것	that (thing)
+무엇/뭐	what
+연필	pencil
+볼펜	(ballpoint) pen
+가위	scissors
+칼	boxcutter
+시계	clock/watch
+달력	calendar
+컴퓨터	computer
+안경	glasses
+휴대폰/휴대전화	cell phone
+계산기	calculator
+컵	cup
+휴지	toilet paper
+가족사진	family picture
+여권	passport
+지갑	wallet
+열쇠	key
+가방	bag
+우산	umbrella
+화장품	cosmetics (skin care)
+거울	mirror
+빗	brush
+베개	pillow
+이불	blanket
+이건 뭐예요?	What is this?
+네, 반장님. 내일 뵙겠습니다.	Yes, Banjangnim. I'll see you tomorrow.
+    `,
+
+    "Lesson 4": `
+일하다	to work
+공부하다	to study
+운동하다	to exercise
+전화하다	to call
+요리하다	to cook
+쇼핑하다	to shop
+가다	to go
+하다	to do
+오다	to come
+밥	rice
+먹다	to eat
+사과	apple
+텔레비전	television
+보다	to watch
+지금	now
+오늘	today
+빵	bread
+과자	snack
+영화	movie
+물	water
+마시다	to drink
+우유	milk
+읽다	to read
+책	book
+주스	juice
+커피	coffee
+신문	newspaper
+어서 오세요.	Welcome.
+    `,
+
+    "Lesson 5": `
+요일	Days of the Week
+일요일	Sunday
+토요일	Saturday
+금요일	Friday
+목요일	Thursday
+수요일	Wednesday
+화요일	Tuesday
+월요일	Monday
+어제	yesterday
+오늘	today
+내일	tomorrow
+평일	weekday
+주말	weekend
+매일	everyday
+날짜 관련 어휘	Dates
+1월 (일월)	January
+2월 (이월)	February
+3월 (삼월)	March
+4월 (사월)	April
+5월 (오월)	May
+6월 (유월)	June
+7월 (칠월)	July
+8월 (팔월)	August
+9월 (구월)	September
+10월 (시월)	October
+11월 (십일월)	November
+12월 (십이월)	December
+이번 달	this month
+    `
+};
